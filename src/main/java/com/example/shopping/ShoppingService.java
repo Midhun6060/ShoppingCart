cc66209cba923deedac847a7c07163e308ce6cf7
Midhun6060 <midhuny417@gmail.com>
2022-09-14 19:12:09 -0400

    Step-2 Simple Offer

--- src/main/java/com/example/shopping/ShoppingService.java
+++ src/main/java/com/example/shopping/ShoppingService.java
@@ -10,12 +10,12 @@ public class ShoppingService {
 	OrderResponse placeOrder(OrderRequest order) {
 		OrderResponse resp = new OrderResponse();
 		if(order.getApples() > 0) {
-			resp.setApples(order.getApples());
+			resp.setApples(order.getApples() * 2);
 			resp.setApplesCost(order.getApples() * this.appleCost);
 		} 
 		if(order.getOranges() > 0) {
 			resp.setOranges(order.getOranges());
-			resp.setOrangesCost(order.getOranges() * this.orangeCost);
+			resp.setOrangesCost(((order.getOranges()%3) * this.orangeCost) + ((order.getOranges()/3) * this.orangeCost * 2));
 		}
 		resp.setTotalCost(resp.getApplesCost() + resp.getOrangesCost());
 		return resp;
